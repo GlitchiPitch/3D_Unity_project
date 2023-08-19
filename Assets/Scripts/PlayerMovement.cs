d1cf53ca56d164d252192293afdd73ed5e39dcfb
GlitchiPitch <141921165+GlitchiPitch@users.noreply.github.com>
2023-08-19 13:34:44 +0500

    update jump and add check ground

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
     [SerializeField] private float movementSpeed = 6f;
     [SerializeField] private float jumpForce = 5f;
+
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask ground;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,10 +22,14 @@ public class PlayerMovement : MonoBehaviour
 
         rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
         }
-        
+    }
+
+    bool IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, .1f, ground);
     }
 }
